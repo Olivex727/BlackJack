da8d7f6691055c68f8e2b9a216c94f3258d3dfb3
Oliver Maxwell Walters <olivermwalters@gmail.com>
2019-09-14 12:02:14 +1000

    Started the card gen

--- imagegen.py
+++ imagegen.py
@@ -1,2 +1,37 @@
+from PIL import Image, ImageFilter, ImageDraw
+
 #Image Generator for cards in pile
 print("hello world")
+
+suite = {"S": "1", "D": "2", "C": "3", "H": "4"}
+num = { "A":"1", "J":"11", "Q":"12", "K":"13"}
+
+#Crops image and converts to .png
+def editImage(n, s):
+    #Get the image files
+    img = Image.open("./public/input/"+n+s+".jpg")
+    new = Image.new('RGBA', (img.size), color=(255, 255, 255, 0))
+    newpixels = new.load()
+    oldpixels = img.load()
+
+    #Translate and trim pixels from source
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            if y > 1 and y < img.size[1] - 2 and x > 1 and x < img.size[0] - 5:
+                newpixels[x, y] = (255 - oldpixels[x, y][0], 255 - oldpixels[x, y][1], 255 - oldpixels[x, y][2], 255)
+
+    #Save new image file
+    new.save("./public/cards/"+suite[s]+"_"+num[n]+".png")
+    new.close()
+    img.close()
+
+#Execution code
+for s in range(2, 11):
+    print(s)
+    num[str(s)] = str(s)
+
+for s in suite:
+    print('Creating Suite: ' + s)
+    for n in num:
+        print('Card: ' + n+s)
+        editImage(n, s)
